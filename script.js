cc7121bc3d02d53100d74fcc78499d91b5e47ae6
PrasadRS <prasad.shete728@gmail.com>
2023-04-02 15:29:46 +0530

    load final version of working calculator

--- script.js
+++ script.js
@@ -1,28 +1,100 @@
 let frirstNumber, secondNumber, operator;
+let operationDisplay = document.querySelector(".operation");
+let resultDisplay = document.querySelector(".result");
 
 function main() {
+  getClickedButton();
+  setTime();
+}
+
+function getOperationArgs(buttonClicked) {
+  // Getting first number and operator
+  if(isOperator(buttonClicked)) {
+    if(frirstNumber == undefined) {
+      frirstNumber = operationDisplay.textContent;
+      operator = buttonClicked;
+      resultDisplay.textContent = operationDisplay.textContent += buttonClicked;
+      resetOperationDisplay();
+      return;
+  
+    //Getting Second Number and performing operation based on operator that comes after second number
+    } else if(secondNumber == undefined && buttonClicked != "=") { 
+      
+      if(operationDisplay.textContent == "") {
+        frirstNumber = resultDisplay.textContent;
+        operator = buttonClicked;
+        resultDisplay.textContent += buttonClicked;
+      } else {
+        secondNumber = operationDisplay.textContent;
+        resultDisplay.textContent = operate(frirstNumber, secondNumber, operator);
+        
+        frirstNumber = resultDisplay.textContent
+        resultDisplay.textContent += buttonClicked;
+        resetOperationDisplay();
+  
+        secondNumber = undefined;
+        operator = buttonClicked;
+      }
+     
+      return;
+    
+    // Calculating results on "=" buttons press.
+    } else if(secondNumber == undefined && buttonClicked == "=") {
+      
+      secondNumber = operationDisplay.textContent;
+      resultDisplay.textContent = operate(frirstNumber, secondNumber, operator);
+      resetOperationDisplay();
+      operator = undefined;
+      secondNumber = undefined;
+      return;
+    }
+    
+  } else {
+    operationDisplay.textContent += buttonClicked;
+  }
+}
+
+function getClickedButton() {
+  const buttons = document.querySelectorAll(".btn.active");
+  let buttonClicked;
+  buttons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      buttonClicked = `${btn.getAttribute("data-key")}`;
+      
+      //checking if = or any other buttn is pressed
+      if(buttonClicked == "clear") {
+        resetCalculator(); 
+      } else {
+        getOperationArgs(buttonClicked);
+      }
+    });
+  });
 
 }
 
-
 function operate(frirstNumber, secondNumber, operator) {
+  let result;
+  frirstNumber = Number(frirstNumber);
+  secondNumber = Number(secondNumber);
+  
   switch (operator) {
     case "+":
-      add(frirstNumber, secondNumber);
+      result = add(frirstNumber, secondNumber);
       break;
     
     case "-":
-      substract(frirstNumber, secondNumber);
+      result = substract(frirstNumber, secondNumber);
       break;
 
     case "*":
-      multiply(frirstNumber, secondNumber);
+      result = multiply(frirstNumber, secondNumber);
       break;
 
     case "/":
-      divide(frirstNumber, secondNumber);
+      result = divide(frirstNumber, secondNumber);
       break;
   }
+  return result;
 }
 
 function add(frirstNumber, secondNumber) {
@@ -38,5 +110,32 @@ function multiply(frirstNumber, secondNumber) {
 }
 
 function divide(frirstNumber, secondNumber) {
+  if (secondNumber == 0) return "Error";
   return frirstNumber / secondNumber;
+}
+
+function resetOperationDisplay() {
+  operationDisplay.textContent = "";
+}
+
+function resetCalculator() {
+  operationDisplay.textContent = "";
+  resultDisplay.textContent = "";
+  frirstNumber = undefined;
+  secondNumber = undefined;
+  operator = undefined;
+}
+
+function isOperator(buttonClicked) {
+  if(buttonClicked == "+" || buttonClicked == "-" || buttonClicked == "*" || buttonClicked == "/" || buttonClicked == "=") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function setTime() {
+  let time = new Date()
+  let timeDiv = document.querySelector(".time");
+  timeDiv.textContent = `${time.getHours()}:${time.getMinutes()}`;
 }
